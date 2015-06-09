462480a397ba790f6f69bf4b1ce740ee0a97f193
joellusky <joelzlusky@gmail.com>
2015-06-09 10:44:38 -0400

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609133923) do
+ActiveRecord::Schema.define(version: 20150609135527) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,7 +28,13 @@ ActiveRecord::Schema.define(version: 20150609133923) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 20150609133923) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
