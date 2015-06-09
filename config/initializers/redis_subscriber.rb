003433f36b62b303c06522d4ffc2ef188c79423b
joellusky <joelzlusky@gmail.com>
2015-06-09 08:49:58 -0400

    initial commit

---
+++ config/initializers/redis_subscriber.rb
@@ -0,0 +1,18 @@
+require 'redis'
+require 'uri'
+require "active_support/core_ext/string/conversions"
+
+url = URI.parse "http://127.0.0.1:6379"
+redis = Redis.new host: url.host, port: url.port, password: url.password
+puts "Redis subscriber running"
+
+Thread.new do
+	redis.subscribe "KUAS_TEST" do |on|
+	  on.message do |event, data|
+
+	  	p Time.now.to_f - data["time"].to_time.to_f
+	  end
+	end
+end
+
+puts "rails continues loading"
